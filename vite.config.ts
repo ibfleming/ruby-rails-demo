72e75002c1c3465b03dcf273afb7d83099bc24dd
Ian Fleming <ianfleming678@gmail.com>
2025-10-17 04:13:36 +0000

    refactor: update Vite configuration and package files to enhance React integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import RailsPlugin from "vite-plugin-rails";
 
 export default defineConfig({
-  plugins: [RailsPlugin()],
+  plugins: [react(), RailsPlugin()],
 });
